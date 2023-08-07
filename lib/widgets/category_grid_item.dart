d7fe17f5a6f55ff4bcda394cffe99f8b443f93a4
mnengineer <masashiii0115@gmail.com>
2023-08-07 23:22:36 +0700

    feat: CategoryItemWidgetの作成

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
