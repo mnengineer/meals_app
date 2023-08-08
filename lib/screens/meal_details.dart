5ef5f33f12d3b8677db13a0bfe816d59853477e2
mnengineer <masashiii0115@gmail.com>
2023-08-08 16:01:30 +0700

    feat: 食事詳細画面のUI修正

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -12,14 +12,58 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(meal.title),
-        ),
-        body: Image.network(
-          meal.imageUrl,
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: 300,
-        ));
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            meal.imageUrl,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 300,
+          ),
+          const SizedBox(height: 14),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 14),
+          for (final ingredient in meal.ingredients)
+            Text(
+              ingredient,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          const SizedBox(height: 24),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 14),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 16,
+              ),
+              child: Text(
+                step,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
