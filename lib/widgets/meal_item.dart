30b71b3f2f861d0bd0e78a7f9755cd16c15b14a6
mnengineer <masashiii0115@gmail.com>
2023-08-08 12:37:02 +0700

    feat: 食事アイテムのUI実装

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -20,7 +20,39 @@ class MealItem extends StatelessWidget {
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
-            )
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 44,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
